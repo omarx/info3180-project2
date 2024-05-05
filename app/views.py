ca9c938fc9d21006efa361befbb179187497b3cf
Omar Peart <omarpeart@Omars-MacBook-Pro.local>
2024-05-04 20:42:12 -0500

    Fixed backend api routes

--- app/views.py
+++ app/views.py
@@ -75,35 +75,41 @@ def register():
 def login():
     if not request.is_json:
         return jsonify({"msg": "Missing JSON in request"}), 400
+
     username = request.json.get('username', None)
     password = request.json.get('password', None)
+
     if not username or not password:
         return jsonify({"msg": "Missing username or password"}), 400
+
     user = Users.query.filter_by(username=username).first()
     if user and check_password_hash(user.password, password):
-        access_token = create_access_token(identity=username, expires_delta=timedelta(days=10))
+        access_token = create_access_token(identity={"username": username, "user_id": user.id},
+                                           expires_delta=timedelta(days=10))
         return jsonify(access_token=access_token, user_id=user.id), 200
     else:
         return jsonify({"msg": "Bad username or password"}), 401
 
 
-@app.route('/api/v1/posts/new', methods=['POST'])
+@app.route('/api/v1/users/<int:user_id>/posts', methods=['POST'])
 @jwt_required()
-def new_post():
+def new_post(user_id):
     form = PostForm()
     if form.validate_on_submit():
         filename = None
         if form.photo.data:
             filename = secure_filename(form.photo.data.filename)
             form.photo.data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
         post = Posts(
             caption=form.caption.data,
             photo=filename,
-            user_id=form.user_id.data,
+            user_id=user_id,
         )
         db.session.add(post)
         db.session.commit()
         return jsonify({"message": "Post created"}), 201
+
     else:
         return jsonify({"errors": form_errors(form)}), 400
 
@@ -136,6 +142,60 @@ def get_posts():
 
     return jsonify({"posts": posts_list}), 200
 
+
+@app.route('/api/v1/users/<int:user_id>/posts', methods=['GET'])
+@jwt_required()
+def get_users_posts(user_id):
+    # Query to fetch posts along with their likes count for a specific user
+    posts = db.session.query(
+        Posts.id,
+        Posts.caption,
+        Posts.photo,
+        Posts.created_at,
+        Users.username,
+        Users.profile_photo,
+        func.count(Likes.id).label('likes_count')
+    ).select_from(Posts
+                  ).join(Users
+                         ).outerjoin(Likes, Likes.post_id == Posts.id
+                                     ).filter(Users.id == user_id
+                                              ).group_by(Posts.id, Users.id
+                                                         ).all()
+    posts_list = [{
+        "caption": post.caption,
+        "photo": post.photo,
+        "username": post.username,
+        "profile_photo": post.profile_photo,
+        "likes_count": post.likes_count,
+        "created_at": post.created_at.strftime('%Y-%m-%d %H:%M:%S')
+    } for post in posts]
+
+    return jsonify({"posts": posts_list}), 200
+
+
+@app.route('/api/v1/posts/<int:post_id>/like', methods=['POST'])
+@jwt_required()
+def like_post(post_id):
+    identity = get_jwt_identity()
+    user_id = identity['user_id']
+
+    post = db.session.query(Posts).filter(Posts.id == post_id).first()
+    if not post:
+        return jsonify({'message': 'Post not found'}), 404
+
+    existing_like = db.session.query(Likes).filter_by(post_id=post_id, user_id=user_id).first()
+    if existing_like:
+        return jsonify({'message': 'You have already liked this post'}), 409
+
+    new_like = Likes(
+        post_id=post_id,
+        user_id=user_id,
+        created_at=datetime.now()
+    )
+    db.session.add(new_like)
+    db.session.commit()
+
+    return jsonify({'message': 'Like added'}), 201
 
 ###
 # The functions below should be applicable to all Flask apps.
